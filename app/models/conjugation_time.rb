a1e6e80e74e58d22cc8126103f398334471bd853
Ulf Moehring <Ulf@los-angeles.fritz.box>
2009-09-27 22:43:16 +0200

    Fixed conjugation join model

--- app/models/conjugation_time.rb
+++ app/models/conjugation_time.rb
@@ -10,7 +10,7 @@ class ConjugationTime < ActiveRecord::Base
       find(:all, :conditions => 'regular = 0')
     end
   end
-  has_many :verbs, :finder_sql => 'SELECT vocabularies.* FROM vocabularies, conjugation_times, conjugations, conjugations_verbs WHERE vocabularies.type = \'Verb\' AND conjugation_times.id = conjugations.id AND conjugations.id = conjugations_verbs.conjugation_id AND conjugations_verbs.verb_id = vocabularies.id AND conjugation_times.id = #{id}'
+  has_many :verbs, :finder_sql => 'SELECT vocabularies.* FROM vocabularies LEFT JOIN conjugations_verbs ON conjugations_verbs.verb_id = vocabularies.id LEFT JOIN conjugations ON conjugations.id = conjugations_verbs.conjugation_id LEFT JOIN conjugation_times ON conjugation_times.id = conjugations.conjugation_time_id WHERE conjugation_times.id = #{id}'
   
   # Validations
   validates_presence_of :name
