2bd69897d009750fecae71a3aa656c489bc61c41
Ulf Moehring <Ulf@los-angeles.fritz.box>
2009-10-12 23:26:58 +0200

    Now includes Google Analytics tracking w/ improved spacing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,12 +32,12 @@ HOST = "tuvocabulario.com"
 
 # Google Analytics tracking
 ANALYTICS_CODE = "<script type=\"text/javascript\">
-                    var gaJsHost = ((\"https:\" == document.location.protocol) ? \"https://ssl.\" : \"http://www.\");
-                    document.write(unescape(\"%3Cscript src='\" + gaJsHost + \"google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E\"));
-                  </script>
-                  <script type=\"text/javascript\">
-                    try {
-                      var pageTracker = _gat._getTracker(\"UA-1190989-3\");
-                      pageTracker._trackPageview();
-                    } catch(err) {}
-                  </script>"
+  var gaJsHost = ((\"https:\" == document.location.protocol) ? \"https://ssl.\" : \"http://www.\");
+  document.write(unescape(\"%3Cscript src='\" + gaJsHost + \"google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E\"));
+</script>
+<script type=\"text/javascript\">
+  try {
+    var pageTracker = _gat._getTracker(\"UA-1190989-3\");
+    pageTracker._trackPageview();
+  } catch(err) {}
+</script>"
