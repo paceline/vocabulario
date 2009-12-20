1950430e54af713d7d54fa27ade39f5ad334cb6e
Paceline <hello@ulfmoehring.net>
2009-12-20 23:57:14 +0100

    Switched from DateTime to Timestamp

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -95,7 +95,7 @@ class UsersController < Clearance::UsersController
   # Show updates for current user
   def timeline
     user = params.key?(:id) ? User.find(params[:id]) : current_user
-    @timeline = params.key?(:since) ? user.timeline(DateTime.parse(params[:since])) : user.timeline
+    @timeline = params.key?(:since) ? user.timeline(Time.at(params[:since].to_f)) : user.timeline
     respond_to do |format|
       format.atom  { render :layout => false }
       format.json { render :json => @timeline }
