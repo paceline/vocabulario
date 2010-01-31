0a35609dc363dab9952dd8ad9df92fd382d3e4d5
Paceline <hello@ulfmoehring.net>
2010-01-31 23:02:42 +0100

    Regular is not a required field

--- app/models/conjugation.rb
+++ app/models/conjugation.rb
@@ -5,7 +5,7 @@ class Conjugation < ActiveRecord::Base
   has_and_belongs_to_many :verbs
   
   # Validations
-  validates_presence_of :conjugation_time_id,	:name, :regular, :first_person_singular,	:second_person_singular, :third_person_singular, :first_person_plural, :second_person_plural, :third_person_plural
+  validates_presence_of :conjugation_time_id,	:name, :first_person_singular, :second_person_singular, :third_person_singular, :first_person_plural, :second_person_plural, :third_person_plural
   
   def regular_or_irregular
     self.regular? ? "regular" : "irregular"
