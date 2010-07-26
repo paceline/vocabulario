b75abeed08ad42aa5eb85bf90791c46f1ac58702
Paceline <hello@ulfmoehring.net>
2010-07-26 20:30:48 +0200

    Upgraded to Rails version 2.3.8

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -15,14 +15,16 @@ Rails::Initializer.run do |config|
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
   # Specify gems that this application depends on and have them installed with rake gems:install
+  config.gem "acts_as_list"
   config.gem "fastercsv"
-  config.gem "justinfrench-formtastic", :lib => 'formtastic', :source  => 'http://gems.github.com'
-  config.gem 'mislav-will_paginate', :lib => 'will_paginate', :source => 'http://gems.github.com'
+  config.gem "haml"
+  config.gem "formtastic"
+  config.gem 'will_paginate'
   config.gem "oauth"
   config.gem "oauth-plugin"
   config.gem "RedCloth"
-  config.gem "thoughtbot-factory_girl", :lib => "factory_girl", :source => "http://gems.github.com"
-  config.gem "thoughtbot-clearance", :lib => 'clearance', :source  => 'http://gems.github.com'
+  config.gem "factory_girl"
+  config.gem "clearance"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
