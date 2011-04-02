e363e8ab92ec92d62b79d1856df6cb2d9fe22334
paceline <hello@ulfmoehring.net>
2011-04-02 22:54:11 +0200

    Rewrote article detection

--- app/models/noun.rb
+++ app/models/noun.rb
@@ -1,22 +1,18 @@
 class Noun < Vocabulary
   
+  # Supported articles
+  ARTICLES = ["la","le","el","il","l'","lo","i","der","die","das","the","les"]
+  
   # Automatically interpret word
   def word=(value)
     if (tmp = value.split(',')).size > 1
-      write_attribute(:word, tmp[0].strip)
-      write_attribute(:gender, tmp[1].strip)
+      write_attribute(:gender, tmp[tmp.size-1].strip)
+      write_attribute(:word, tmp[0..tmp.size-2].join(',').strip)
+    elsif (article = find_matching_articles(value))
+      write_attribute(:gender, article)
+      write_attribute(:word, value[article.size..value.size-1].strip)
     else
-      if (tmp = value.split('\'')).size > 1
-        write_attribute(:gender, "#{tmp[0].strip}'")
-        write_attribute(:word, tmp[1].strip)
-      else
-        if (tmp = value.split(' ')).size > 1
-          write_attribute(:gender, tmp[0].strip)
-          write_attribute(:word,value.sub(self.gender,'').strip)
-        else
-          write_attribute(:word, tmp[0])
-        end
-      end
+      write_attribute(:word, value)
     end
   end
   
@@ -24,5 +20,13 @@ class Noun < Vocabulary
   def word
     gender? ? (gender.include?('\'') ? "#{gender}#{read_attribute(:word)}" : "#{gender} #{read_attribute(:word)}") : read_attribute(:word)
   end
+  
+  private
+    def find_matching_articles(value)
+      ARTICLES.each do |article|
+        return article if article.casecmp(value[0..article.size-1]) == 0
+      end
+      return false
+    end
 
 end
